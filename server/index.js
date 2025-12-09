6e4860035a6927cf78bc61474c06210d3dbe9784
Aman ____ <aman.tshekar@gmail.com>
2025-12-10 00:17:13 +0530

    Add request logging for debugging

--- server/index.js
+++ server/index.js
@@ -29,6 +29,10 @@ console.error = (...args) => {
 };
 
 // Middleware
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
 app.use(cors());
 app.use(express.json());
 
